35fce2b43300381834474490ac3576f1464014f5
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-09-11 13:19:42 +0200

    Style switch element and display toggle text

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,20 +1,46 @@
-import { ListItem, ListItemProps, Switch } from '@rneui/themed';
+import { Switch, ThemeProvider, createTheme } from '@rneui/themed';
+import { Text, StyleSheet } from 'react-native';
 import React, { useState } from 'react';
+
+const theme = createTheme({
+    components: {
+        Switch: {
+            translucent: true,
+            transform: [{ scaleX: 3 }, { scaleY: 3 }],
+            color: '#e4e3d3',
+            raised: true,
+            paddingBottom: 10,
+        },
+    },
+});
 
 export default function ToggleElement () {
 
-    const [switch1, setSwitch1] = useState(true);
+    const [switchValue, setSwitchValue] = useState(false);
+
+    const toggleSwitch = (value) => {
+        setSwitchValue(value);
+    };
 
     return (
-        <ListItem bottomDivider>
-            <ListItem.Content>
-                <ListItem.Title>Switch that please 😲</ListItem.Title>
-            </ListItem.Content>
+        <ThemeProvider theme={theme}>
             <Switch
-                value={switch1}
-                onValueChange={(value) => setSwitch1(value)}
+                value={switchValue}
+                onValueChange={toggleSwitch}
             />
-        </ListItem>
+            <Text style={styles.textstyle}>{switchValue ? 'Switch is ON' : 'Switch is OFF'}</Text>
+        </ThemeProvider>
 
     )
 };
+
+const styles = StyleSheet.create({
+    textstyle: {
+        fontWeight: 'bold',
+        color: '#e4e3d3',
+        fontSize: 25,
+        paddingVertical: 5,
+        paddingBottom: 20,
+        alignItems: 'center'
+    }
+});
