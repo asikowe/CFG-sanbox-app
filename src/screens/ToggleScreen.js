7e7766df7ecb18b9da8ad64e6951579f51aff80d
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-09-11 12:40:55 +0200

    Add fourth screen

---
+++ src/screens/ToggleScreen.js
@@ -0,0 +1,45 @@
+import { ImageBackground, StyleSheet, Text, View, StatusBar } from 'react-native';
+
+export default function Home() {
+    return (
+        <View style={styles.container1}>
+            <StatusBar style="dark-content" backgroundColor="#000000" translucent={true} />
+            <ImageBackground source={require('../../assets/road.jpg')} resizeMode='cover' style={styles.image}>
+                <View style={styles.container2}>
+                    <Text style={styles.title1}>HI JOANNA</Text>
+                    <Text style={styles.title2}>Here's toggle element:</Text>
+                </View>
+            </ImageBackground>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container1: {
+        flex: 1
+    },
+    container2: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    title1: {
+        fontWeight: 'bold',
+        color: '#e4e3d3',
+        fontSize: 35,
+        paddingVertical: 5,
+        alignItems: 'center'
+    },
+    title2: {
+        fontWeight: 'bold',
+        color: '#e4e3d3',
+        fontSize: 25,
+        paddingVertical: 5,
+        paddingBottom: 20,
+        alignItems: 'center'
+    }
+});
