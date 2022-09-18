3c1f0b3fe8c98cab40c27bb7b2b2c7ba7d2a3ca8
bodl3178 <joanna.bek@bodleian.ox.ac.uk>
2022-09-18 21:42:08 +0200

    Add navigation based on logged status

--- src/components/NavigationFile.js
+++ src/components/NavigationFile.js
@@ -2,14 +2,19 @@ import Home from '../screens/Home'
 import AnotherFile from '../screens/SomeFile';
 import SomeClass from '../screens/MountingFile';
 import ScreenToggling from '../screens/ToggleScreen';
+import LoggingIn from '../screens/Login';
+import Register from '../screens/Register';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
+import { useSelector, useDispatch } from 'react-redux';
 
 const Tab = createBottomTabNavigator();
 
 export default function AppNavigation() {
+
+    const isSignedIn = useSelector(state => state.isAuthenticated)
+
     return (
         <NavigationContainer>
             <Tab.Navigator
@@ -32,19 +37,32 @@ export default function AppNavigation() {
                     iconName = focused ? 'bug' : 'bug-outline';
                 } else if (route.name === 'Toggle') {
                     iconName = focused ? 'toggle' : 'toggle-outline';
+                } else if (route.name === 'Log In') {
+                    iconName = focused ? 'log-in' : 'log-in-outline';
+                } else if (route.name === 'Register') {
+                    iconName = focused ? 'newspaper' : 'newspaper-outline';
                 }
-
-                // You can return any component that you like here!
+                
                 return <Ionicons name={iconName} size={size} color={color} />;
             },
                 tabBarActiveTintColor: '#e4e3d3',
                 tabBarInactiveTintColor: 'gray',
+                tabBarHideOnKeyboard: true,
 
             })}>
+                {isSignedIn ? (
+                <>
                 <Tab.Screen name="Home" component={Home} />
                 <Tab.Screen name="Space" component={AnotherFile} />
                 <Tab.Screen name="State" component={SomeClass} />
                 <Tab.Screen name="Toggle" component={ScreenToggling} />
+                </>
+                ) : (
+                <>
+                    <Tab.Screen name="Log In" component={LoggingIn} />
+                    <Tab.Screen name="Register" component={Register} />
+                </>
+                )}
             </Tab.Navigator>
         </NavigationContainer>
     );
